7e3878c2cac62158b1413435aacc36913c533f51
Aron Wei Zhe <64115015+MystixWEIZHE@users.noreply.github.com>
2020-06-18 14:26:19 +0800

    Created By Aron

---
+++ 13_Aron_PA11/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -5.5f, 5.5f);
+        transform.position = clampedPosition;
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+        }
+    }   
+}
---
+++ 13_Aron_PA11/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] SpawnObject;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        int randomNumber;
+        randomNumber = Random.Range(0,SpawnObject.Length);
+        PositionY = Random.Range(4f, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(SpawnObject[randomNumber], transform.position, transform.rotation);
+    }
+}
